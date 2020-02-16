143e982bd3c10dd31b7eea03cfafc2de000d603f
Jarred Parr <jparr721@gmail.com>
2020-02-16 13:33:09 -0700

    thread pool impl done

--- threading/include/threading/pool.h
+++ threading/include/threading/pool.h
@@ -1,38 +1,88 @@
 #pragma once
 
 #include <algorithm>
+#include <functional>
+#include <future>
 #include <iterator>
+#include <mutex>
+#include <queue>
+#include <stdexcept>
 #include <thread>
+#include <type_traits>
 #include <vector>
 
 namespace wasabi {
-template <class F>
 class Pool {
+ public:
+  Pool(size_t pool_size) : num_threads_(pool_size), stop(false) {
+    for (size_t i = 0; i < this->num_threads_; ++i) {
+      this->workers.emplace_back([this] {
+        for (;;) {
+          std::function<void> task;
+          {
+            std::unique_lock<std::mutex> lock(this->queue_mutex);
+            this->condition.wait(
+                lock, [this] { return this->stop || !this->tasks.empty() });
+            if (this->stop && this->tasks.empty()) {
+              return;
+            }
+
+            task = std::move(this->tasks.front());
+            this->tasks.pop();
+          }
+
+          task();
+        }
+      });
+    }
+  }
+  ~Pool() {
+    {
+      std::unique_lock<std::mutex> lock(queue_mutex);
+      this->stop = true;
+    }
+
+    condition.notify_all();
+
+    for (auto& thread : this->workers) {
+      thread.join();
+    }
+  }
+
+  //==========================================================
+  template <class F, class... Args>
+  inline std::future<typename std::result_of<F(Args...)>::type> AddJob(
+      F&& f, Args&&... args) {
+    auto task = std::make_shared<
+        std::packaged_task<typename std::result_of<F(Args...)>::type()>>(
+        std::bind(std::forward<F>(f), std::forward<Args>(args)...));
+
+    std::future<typename std::result_of<F(Args...)>::type> res =
+        task->get_future();
+    {
+      std::unique_lock<std::mutex> lock(queue_mutex);
+
+      if (stop) {
+        throw std::runtime_error("Cannot add job to stopped queue");
+      }
+
+      tasks.emplace([task]() { (*task)(); });
+    }
+
+    condition.notify_one();
+    return res;
+  }
+
  private:
   bool stop;
   const int num_threads_;
 
   //==========================================================
   std::condition_variable condition;
+  std::mutex queue_mutex;
 
   //==========================================================
-  std::vector<std::thread<F>> workers(num_threads);
-
- public:
-  Pool(size_t pool_size) : num_threads_(pool_size), stop(false){};
-  ~Pool() {
-    condition.notify_all();
-
-    for (auto& thread : workers) {
-      thread.join();
-    }
-  }
-
-  //==========================================================
-  inline void FromThreadPool(const std::vector<std::thread<F>>& pool) {
-    std::copy(workers.begin(), workers.end(), std::back_inserter(pool));
-  }
-
-  void AddJob(std::thread) {}
+  std::vector<std::thread> workers;
+  std::queue<std::function<void()>> tasks;
 };
 }  // namespace wasabi
